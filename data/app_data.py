039d301cfdd41607755a7db7f0800a0e9bf424e8
Tim Gormly <89597142+tgormly@users.noreply.github.com>
2023-07-13 20:47:24 -0500

    Added code to load iris data set and generate xlsx & csv files

--- data/app_data.py
+++ data/app_data.py
@@ -40,6 +40,9 @@ flights_df.to_csv(data_folder.joinpath("flights.csv"))
 mtcars_df = pd.read_csv(data_folder.joinpath("mtcars.csv"))
 mtcars_df.to_excel(data_folder.joinpath("mtcars.xlsx"))
 
+iris_df = sns.load_dataset("iris")
+iris_df.to_excel(data_folder.joinpath("iris.xlsx"))
+iris_df.to_csv(data_folder.joinpath("iris.csv"))
 
 # url="https://webpath/to/your/data.csv"
 # df=pd.read_csv(url)
